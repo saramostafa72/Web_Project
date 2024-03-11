dc9819b3b6344b455978ed010c9cc2efcfd243b8
saramostafa72 <99365927+saramostafa72@users.noreply.github.com>
2024-03-11 23:27:16 +0200

    Add files via upload

---
+++ Web Project/js/style.js
@@ -0,0 +1,11 @@
+document.getElementById('butn-login').addEventListener("click",()=>{
+    window.location.href("index.html")
+ })
+const switchers = [...document.querySelectorAll('.switcher')]
+
+switchers.forEach(item => {
+	item.addEventListener('click', function() {
+		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
+		this.parentElement.classList.add('is-active')
+	})
+})
